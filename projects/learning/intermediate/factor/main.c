4a7b864939bf0af1335e3af08c5ed5109e7daa05
standardloop <joshuaeli1234@gmail.com>
2024-05-11 23:56:19 -0600

    re-org

---
+++ projects/learning/intermediate/factor/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 1)
+    {
+        printf("Please provide at least 1 argument\n");
+        return EXIT_FAILURE;
+    }
+
+    int number_to_factor = atoi(argv[1]);
+
+    int i = 1;
+
+    // linked list
+    // while ()
+    // {
+    // }
+
+    return EXIT_SUCCESS;
+}
