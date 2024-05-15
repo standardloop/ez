0011bffc177381a16bb5cf48692b4c14899242d1
standardloop <joshuaeli1234@gmail.com>
2024-05-14 21:26:32 -0600

    update list problems

--- projects/learning/novice/array/main.c
+++ projects/learning/novice/array/main.c
@@ -5,17 +5,31 @@ void printArr(int[], int);
 int searchArr(int[], int, int);
 int minArr(int[], int);
 
+int getFirstArrayElement(int[]);
+int getLastArrayElement(int[], int);
+
+void printDivider();
+
 void printArr(int arr[], int n)
 {
+    printf("Printing Array:\n");
+    printf("[");
     for (int i = 0; i < n; i++)
     {
-        printf("%d ", arr[i]);
+        if (i == n - 1)
+        {
+            printf("%d]\n", arr[i]);
+        }
+        else
+        {
+            printf("%d, ", arr[i]);
+        }
     }
-    printf("\n");
 }
 
 int searchArr(int arr[], int n, int x)
 {
+    printf("Searching for %d ...\n", x);
     for (int i = 0; i < n; i++)
     {
         if (arr[i] == x)
@@ -39,6 +53,7 @@ int maxArr(int arr[], int n)
     }
     return max;
 }
+
 int minArr(int arr[], int n)
 {
     int min = arr[0];
@@ -52,18 +67,44 @@ int minArr(int arr[], int n)
     return min;
 }
 
+int getFirstArrayElement(int arr[])
+{
+    return arr[0];
+}
+
+int getLastArrayElement(int arr[], int n)
+{
+    return arr[n - 1];
+}
+
+void printDivider()
+{
+    printf("-------------------------------------------\n");
+}
+
 int main(void)
 {
-    int arr[] = {2, 3, 4, 10, 40};
+    printDivider();
+    int arr[] = {2, 12323, 3, 4, 123213, 10, 40, 877672, 42, 1, 6023, 88832};
     int x = 10;
     int n = sizeof(arr) / sizeof(arr[0]);
 
+    printArr(arr, n);
+    printDivider();
+
     int result = searchArr(arr, n, x);
     result != -1 ? printf("Element is present at index %d\n", result) : printf("Element is not present in array\n");
+    printDivider();
     result = searchArr(arr, n, -3);
     result != -1 ? printf("Element is present at index %d\n", result) : printf("Element is not present in array\n");
+    printDivider();
     printf("maxArr element is: %d\n", maxArr(arr, n));
+    printDivider();
     printf("minArr element is: %d\n", minArr(arr, n));
+    printDivider();
+    printf("[0]'th (First) Element %d\n", getFirstArrayElement(arr));
+    printDivider();
+    printf("[n-1]'th (Last) Element %d\n", getLastArrayElement(arr, n));
 
     return 0;
 }
